b81446a3ab99078de73ff517c7074d2b6fd5dc2d
yeheum Choi <61037219+cyheum@users.noreply.github.com>
2022-04-28 02:27:58 +0900

    [ENH] : 납품실적 목록 삭제,수정,추가 기능 완료 (#3)

--- src/components/Layout/Company/Performance/AddPerformance/index.style.tsx
+++ src/components/Layout/Company/Performance/AddPerformance/index.style.tsx
@@ -1,1 +1,109 @@
 import styled from 'styled-components';
+import { mixins } from '@/styles';
+
+export const Container = styled.div`
+  margin-right: 10%;
+
+  h1 {
+    margin-bottom: 24px;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 43px;
+    color: #383838;
+  }
+
+  @media (max-width: 1682px) {
+    margin-right: 12%;
+  }
+
+  @media (max-width: 1023px) {
+    margin-right: 0;
+  }
+`;
+
+export const AddContainer = styled.div`
+  ${mixins.flexSet('flex-start')}
+  padding: 24px;
+  margin-bottom: 12px;
+  background: #ffffff;
+  box-shadow: 2px 4px 12px 4px rgba(56, 56, 56, 0.08);
+  border-radius: 13px;
+  overflow-x: auto;
+`;
+
+export const AddButton = styled.div`
+  ${mixins.flexSet('flex-end')}
+  margin-bottom: 48px;
+
+  button {
+    width: 90px;
+    height: 48px;
+    background: #2979ff;
+    border-radius: 8px;
+    font-weight: 600;
+    font-size: 17px;
+    line-height: 24px;
+    color: white;
+  }
+`;
+
+export const ItemWrapper = styled.div<{ marginRight?: number }>`
+  margin-right: ${({ marginRight }) => marginRight ?? 16}px;
+`;
+
+export const SubTitle = styled.h4`
+  margin-bottom: 4px;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  color: #383838;
+`;
+
+export const InputBox = styled.input<{ width?: number }>`
+  width: ${({ width }) => width ?? 100}px;
+  height: 48px;
+  padding: 0 16px;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 24px;
+  color: #383838;
+  border: 1px solid #e8e8e8;
+  box-sizing: border-box;
+  border-radius: 8px;
+  background: #fcfcfc;
+`;
+
+type selectBoxProps = {
+  width?: number;
+  isValueOn?: false;
+};
+
+export const SelectBox = styled.div<selectBoxProps>`
+  ${mixins.flexSet('space-between')}
+  position: relative;
+  width: ${({ width }) => width ?? 100}px;
+  border: 1px solid #e8e8e8;
+  box-sizing: border-box;
+  border-radius: 8px;
+  background: #fcfcfc;
+  cursor: pointer;
+
+  .inputStyle {
+    ${mixins.flexSet('flex-start')}
+    width: 100%;
+    height: 48px;
+    padding: 0 16px;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 24px;
+    border: none;
+    color: #383838;
+  }
+
+  .svgStyle {
+    position: absolute;
+    right: 16px;
+    width: 12px;
+    pointer-events: none;
+  }
+`;
--- src/store/sagas/performance.ts
+++ src/store/sagas/performance.ts
@@ -1,7 +1,12 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
-import { API, DELIVERY_API } from '@/utils';
+import { API, DELIVERY_LIST_API, DELIVERY_API } from '@/utils';
 import { performanceActions } from '../module/performance';
-import { ActionType, IGetDeliveryListParams } from '@/interfaces';
+import {
+  ActionType,
+  IGetDeliveryListParams,
+  IPostDelivery,
+} from '@/interfaces';
+import { PostAdd } from '@mui/icons-material';
 
 export function* getDeliveryListSaga({
   payload,
@@ -9,7 +14,7 @@ export function* getDeliveryListSaga({
   try {
     const deliveryList = yield call(
       API.GET,
-      `${DELIVERY_API}?skip=${payload.skip}&limit=${payload.limit}${
+      `${DELIVERY_LIST_API}?skip=${payload.skip}&limit=${payload.limit}${
         payload.isAsc ? '' : '&sort=delivery-year+desc&sort=delivery-month+desc'
       }`
     );
@@ -24,8 +29,91 @@ export function* getDeliveryListSaga({
   }
 }
 
+export function* postDeliverySaga({
+  payload,
+}: ActionType & { payload: IPostDelivery }) {
+  try {
+    yield call(API.POST, DELIVERY_API, { bodyData: payload });
+    const deliveryList = yield call(
+      API.GET,
+      `${DELIVERY_LIST_API}?skip=${1}&limit=${10}&sort=delivery-year+desc&sort=delivery-month+desc`
+    );
+    yield put(
+      performanceActions.setDeliveryList({
+        list: deliveryList.data,
+        size: deliveryList.size,
+      })
+    );
+    alert('추가 완료');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function* patchDeliverySaga({
+  payload,
+}: ActionType & { payload: { id: string | number; info: IPostDelivery } }) {
+  try {
+    yield call(API.PATCH, `${DELIVERY_API}/${payload.id}`, {
+      bodyData: payload.info,
+    });
+    const deliveryList = yield call(
+      API.GET,
+      `${DELIVERY_LIST_API}?skip=${1}&limit=${10}&sort=delivery-year+desc&sort=delivery-month+desc`
+    );
+    yield put(
+      performanceActions.setDeliveryList({
+        list: deliveryList.data,
+        size: deliveryList.size,
+      })
+    );
+    yield put(performanceActions.setSelectedInfo(null));
+    alert('수정 완료');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function* deleteDeliverySaga({
+  payload,
+}: ActionType & { payload: { id: number | string } }) {
+  try {
+    yield call(API.DELETE, `${DELIVERY_API}/${payload.id}`);
+    const deliveryList = yield call(
+      API.GET,
+      `${DELIVERY_LIST_API}?skip=${1}&limit=${10}&sort=delivery-year+desc&sort=delivery-month+desc`
+    );
+    yield put(
+      performanceActions.setDeliveryList({
+        list: deliveryList.data,
+        size: deliveryList.size,
+      })
+    );
+    alert('삭제 완료');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export function* watchDeliveryList() {
   yield takeEvery(performanceActions.getDeliveryList, getDeliveryListSaga);
 }
 
-export default [watchDeliveryList].map((fn) => fn());
+export function* watchPostDelivery() {
+  yield takeEvery(performanceActions.postDelivery, postDeliverySaga);
+}
+
+export function* watchPatchDelivery() {
+  yield takeEvery(performanceActions.patchDelivery, patchDeliverySaga);
+}
+
+export function* watchDeleteDelivery() {
+  yield takeEvery(performanceActions.deleteDelivery, deleteDeliverySaga);
+}
+
+export default [
+  watchDeliveryList,
+  watchPostDelivery,
+  watchPatchDelivery,
+  watchDeleteDelivery,
+].map((fn) => fn());
