8df2cfd64fee715e99fed72eab417a0c06b679f6
Gunoo-Kim <gunoo@cherground.com>
2022-07-06 21:43:37 +0900

    children 타입 추가

--- src/components/widgets/layout/ModalLayout.tsx
+++ src/components/widgets/layout/ModalLayout.tsx
@@ -1,36 +1,32 @@
-import * as React from 'react';
-import Dialog from '@mui/material/Dialog';
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
 
 interface ModalLayoutProps {
-  paperStyle?: React.CSSProperties;
-  onClose: (reason: 'backdropClick' | 'escapeKeyDown') => void;
+    children: React.ReactNode;
+    paperStyle?: React.CSSProperties;
+    onClose: (reason: "backdropClick" | "escapeKeyDown") => void;
 }
 
-const ModalLayout: React.FC<ModalLayoutProps> = ({
-  children,
-  paperStyle,
-  onClose,
-}) => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+const ModalLayout: React.FC<ModalLayoutProps> = ({ children, paperStyle, onClose }) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <Dialog
-      open
-      fullScreen={false}
-      maxWidth={false}
-      onClose={(e, r) => {
-        e;
-        onClose(r);
-      }}
-      PaperProps={{
-        style: paperStyle,
-      }}
-    >
-      {children}
-    </Dialog>
-  );
+    return (
+        <Dialog
+            open
+            fullScreen={false}
+            maxWidth={false}
+            onClose={(e, r) => {
+                e;
+                onClose(r);
+            }}
+            PaperProps={{
+                style: paperStyle,
+            }}>
+            {children}
+        </Dialog>
+    );
 };
 
 export default ModalLayout;
