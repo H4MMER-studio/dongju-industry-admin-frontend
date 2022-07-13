df6213edf6c2a9a1482e4fe6e1659fe37c0a0257
Gunoo-Kim <gunoo@cherground.com>
2022-07-13 21:44:49 +0900

    add) editor 생성

---
+++ src/components/Layout/Editor/CKEditor.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+interface IProps {
+    value: string;
+    name: string;
+    editorLoaded: boolean;
+    onChange: (data: any) => void;
+}
+
+const CKEditor: React.FC<IProps> = ({ onChange, editorLoaded, name, value }) => {
+    const editorRef = useRef();
+    const { CKEditor, ClassicEditor } = editorRef.current || {};
+
+    useEffect(() => {
+        editorRef.current = {
+            CKEditor: require("@ckeditor/ckeditor5-react").CKEditor, // v3+
+            ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),
+        };
+    }, []);
+
+    return (
+        <div>
+            {editorLoaded ? (
+                <CKEditor
+                    type=""
+                    name={name}
+                    editor={ClassicEditor}
+                    data={value}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        // console.log({ event, editor, data })
+                        onChange(data);
+                    }}
+                />
+            ) : (
+                <div>Editor loading</div>
+            )}
+        </div>
+    );
+};
+
+export default CKEditor;
--- src/components/Layout/Editor/EditorContainer.tsx
+++ src/components/Layout/Editor/EditorContainer.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import CKEditor from "./CKEditor";
 
 const EditorContainerLayout = styled.div``;
 
 const EditorContainer: React.FC = () => {
-    return <EditorContainerLayout>에디터</EditorContainerLayout>;
+    const [editorLoaded, setEditorLoaded] = useState(false);
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        setEditorLoaded(true);
+    }, []);
+
+    return (
+        <EditorContainerLayout>
+            <CKEditor
+                name="description"
+                onChange={(data) => {
+                    setData(data);
+                }}
+                editorLoaded={editorLoaded}
+            />
+        </EditorContainerLayout>
+    );
 };
 
 export default EditorContainer;
