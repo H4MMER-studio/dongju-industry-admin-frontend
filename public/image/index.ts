989e672bc6c042d4750313e525de690e8e737bc7
Gunoo-Kim <gunoo@cherground.com>
2022-08-10 23:40:19 +0900

    공지사항 디테일

--- public/image/index.ts
+++ public/image/index.ts
@@ -1,35 +1,37 @@
-const EsitmateBackground = '/image/estimate-background.png';
-const ASBackground = '/image/AS-background.png';
-const ETCBackground = '/image/ETC-background.png';
-const InfoBackground = '/image/info-background.png';
-const Sign = '/image/sign.png';
-const CompanyInfoBannerLarge = '/image/company_info_banner_large.png';
-const CompanyInfoBannerSmall = '/image/company_info_banner_small.png';
-const PagenationLeft = '/image/pagenationLeft.png';
-const PagenationRight = '/image/pagenationRight.png';
+const EsitmateBackground = "/image/estimate-background.png";
+const ASBackground = "/image/AS-background.png";
+const ETCBackground = "/image/ETC-background.png";
+const InfoBackground = "/image/info-background.png";
+const Sign = "/image/sign.png";
+const CompanyInfoBannerLarge = "/image/company_info_banner_large.png";
+const CompanyInfoBannerSmall = "/image/company_info_banner_small.png";
+const PagenationLeft = "/image/pagenationLeft.png";
+const PagenationRight = "/image/pagenationRight.png";
 
 export const Images = {
-  EsitmateBackground,
-  ASBackground,
-  ETCBackground,
-  InfoBackground,
-  Sign,
-  CompanyInfoBannerLarge,
-  CompanyInfoBannerSmall,
-  PagenationLeft,
-  PagenationRight,
+    EsitmateBackground,
+    ASBackground,
+    ETCBackground,
+    InfoBackground,
+    Sign,
+    CompanyInfoBannerLarge,
+    CompanyInfoBannerSmall,
+    PagenationLeft,
+    PagenationRight,
 };
 
-import DownloadIcon from './download-icon.svg';
-import ShareIcon from './share-icon.svg';
-import CloseIcon from './close-icon.svg';
-import RightArrow from './right-arrow.svg';
-import LeftArrow from './left-arrow.svg';
+import DownloadIcon from "./download-icon.svg";
+import ShareIcon from "./share-icon.svg";
+import CloseIcon from "./close-icon.svg";
+import RightArrow from "./right-arrow.svg";
+import LeftArrow from "./left-arrow.svg";
+import DownloadIconBlue from "./download-icon-blue.svg";
 
 export const Icon = {
-  DownloadIcon,
-  ShareIcon,
-  CloseIcon,
-  RightArrow,
-  LeftArrow,
+    DownloadIcon,
+    ShareIcon,
+    CloseIcon,
+    RightArrow,
+    LeftArrow,
+    DownloadIconBlue,
 };
